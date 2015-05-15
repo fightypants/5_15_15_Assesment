f0ec9fb1770141ca5e44913585c58f2ca542a2ab
fightypants <kha.les.email@gmail.com>
2015-05-15 09:14:53 -0700

    Will create spec_helper file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+ENV['RACK_ENV'] = 'test'
+
+require('bundler/setup')
+Bundler.require(:default, :test)
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+require('./app')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Category.all().each() do |category|
+      category.destroy()
+    end
+    Recipe.all().each() do |recipe|
+      recipe.destroy()
+    end
+    Ingredient.all().each() do |ingredient|
+      ingredient.destroy()
+    end
+  end
+end
